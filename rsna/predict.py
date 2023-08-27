60c4583bdf3e7741d9c9414f5cbf267ad175ca51
Ubuntu <ubuntu@ip-172-31-4-246.us-west-1.compute.internal>
2023-08-27 23:13:45 +0000

    fix: resolve merge conflicts and update params

--- rsna/predict.py
+++ rsna/predict.py
@@ -1,5 +1,7 @@
 import torch
+import torchvision
 from model import TraumaDetector
+from datasets import RSNADataset
 from params import *
 
 model = TraumaDetector()
@@ -7,5 +9,7 @@ print(model)
 model = model.to(DEVICE)
 
 model.load_state_dict(torch.load('models/rsna.pt'))
+
+test_iter = RSNADataset(csv_file=TEST_FILE, root_dir=TEST_DIR, transform=torchvision.transforms.Compose([torchvision.transforms.Resize((512, 512))])
 
 print(model)
