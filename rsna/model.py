75cde04c27f768e9ec09192e165f0fe322c2e337
joe-lin-tech <joe.tech.lin@gmail.com>
2023-08-31 21:24:20 -0700

    update: remove convolutional layers and clean up architecture

--- rsna/model.py
+++ rsna/model.py
@@ -25,51 +25,16 @@ class CombinedLoss(nn.Module):
         return bce_loss + ce_loss + any_loss
 
 
-class DepthwiseSeparableConv(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride = 1, padding = 0):
-        super(DepthwiseSeparableConv, self).__init__()
-        self.depth_conv = nn.Conv2d(in_channels, in_channels, kernel_size, stride, padding, groups=in_channels)
-        self.point_conv = nn.Conv2d(in_channels, out_channels, 1)
-    
-    def forward(self, x):
-        return self.point_conv(self.depth_conv(x))
-
-
 class TraumaDetector(nn.Module):
     def __init__(self):
         super(TraumaDetector, self).__init__()
-        # self.conv1 = DepthwiseSeparableConv(96, 128, 7, stride=2, padding=3)
-        # self.dropout = nn.Dropout2d()
-        # self.conv2 = nn.Conv2d(128, 64, 7, padding=2, dilation=6)
-        self.conv1 = nn.Conv2d(3, 8, 9, stride=2)
-        self.conv2 = nn.Conv2d(8, 16, 11, stride=1)
-        self.conv3 = nn.Conv2d(16, 8, 11, stride=1)
-        self.conv4 = nn.Conv2d(8, 3, 9, stride=1)
-
-        # backbone = efficientnet_b0(weights=EfficientNet_B0_Weights.IMAGENET1K_V1)
-        backbone = vit_b_16(weights=ViT_B_16_Weights.IMAGENET1K_V1)
-        self.backbone = backbone
+        self.backbone = vit_b_16(weights=ViT_B_16_Weights.IMAGENET1K_V1)
         self.backbone.eval()
-        # self.backbone_proj = backbone.conv_proj
-        # self.cls_token = nn.Parameter(torch.rand(1, 1, 768))
-        # self.backbone_encoder = backbone.encoder
-        # self.backbone_encoder.eval()
+        self.backbone.conv_proj = nn.Conv2d(N_CHANNELS, 768, 16, stride=16)
         for layer in self.backbone.encoder.layers:
             layer.self_attention._reset_parameters()
             layer.self_attention.train()
         self.backbone.heads.head = nn.Linear(768, 384)
-        # self.backbone_encoder.layers.encoder_layer_10.train()
-        # self.backbone_encoder.layers.encoder_layer_10.self_attention._reset_parameters()
-        # self.backbone_encoder.layers.encoder_layer_11.train()
-        # self.backbone_encoder.layers.encoder_layer_11.self_attention._reset_parameters()
-
-        # self.backbone_features = backbone.features[2:]
-        # self.backbone_features[0][0].block = self.backbone_features[0][0].block[1:]
-        # self.backbone_features.eval()
-        # self.backbone_pool = backbone.avgpool
-        # self.backbone_pool.eval()
-        
-        # self.linear = nn.Linear(768, 384)
 
         self.linear_bowel = nn.Linear(384, 192)
         self.linear_extravasation = nn.Linear(384, 192)
@@ -84,20 +49,7 @@ class TraumaDetector(nn.Module):
         self.out_spleen = nn.Linear(192, 3)
     
     def forward(self, x):
-        # x = F.gelu(self.conv2(self.dropout(F.relu(self.conv1(x)))))
-        # x = self.backbone_pool(self.backbone_features(x))
-        # patches = self.img_to_patch(x, 16)
-        x = F.relu(self.conv4(F.relu(self.conv3(F.relu(self.conv2(F.relu(self.conv1(x))))))))
-        # proj = self.backbone_proj(x).flatten(start_dim=2).transpose(1, 2)
-        # cls_token = self.cls_token.repeat(proj.shape[0], 1, 1) # fix cls_token training
-        # proj = torch.cat([cls_token, proj], dim=1)
-        # x = self.backbone_encoder(proj)
         x = self.backbone(x)
-        # x = self.backbone_encoder(self.backbone_proj(x))
-        
-        # x = F.gelu(self.linear(x[:, 0, :]))
-        # x = torch.reshape(x, (x.shape[0], -1))
-
         out = {
             'bowel': self.out_bowel(F.gelu(self.linear_bowel(x))),
             'extravasation': self.out_extravasation(F.gelu(self.linear_extravasation(x))),
--- rsna/params.py
+++ rsna/params.py
@@ -1,7 +1,7 @@
 import torch
 
 BATCH_SIZE = 16
-N_CHANNELS = 3
+N_CHANNELS = 4
 
 SEED = 0
 
@@ -10,7 +10,7 @@ EPOCHS = 25
 
 DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('mps')
 CSV_FILE = 'data/train.csv'
-ROOT_DIR = '../../train_images_mini' # '/Volumes/SSD/rsna/train_images_mini_512x512'
+ROOT_DIR = '/Volumes/SSD/rsna/train_images_mini_256x256' # '/Volumes/SSD/rsna/train_images_mini_512x512'
 TEST_DIR = 'data/test_images'
 
 CHECKPOINT_FOLDER = 'models/checkpoint'
