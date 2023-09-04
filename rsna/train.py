1ac0272502cc856ffe9155b4ce53b2c9e392eea1
joe-lin-tech <joe.tech.lin@gmail.com>
2023-09-03 22:10:09 -0700

    update: remove random transforms from training procedure

--- rsna/train.py
+++ rsna/train.py
@@ -64,8 +64,8 @@ for i, (train_idx, val_idx) in enumerate(splits):
     train_data, val_data = data.iloc[train_idx], data.iloc[val_idx]
     train_iter = RSNADataset(split=train_data, root_dir=ROOT_DIR, transform=torchvision.transforms.Compose([
         torchvision.transforms.Resize((256, 256)),
-        torchvision.transforms.RandomHorizontalFlip(),
-        torchvision.transforms.RandomRotation(degrees=7.5)
+        # torchvision.transforms.RandomHorizontalFlip(),
+        # torchvision.transforms.RandomRotation(degrees=7.5)
     ]), input_type='jpeg')
     train_sampler = WeightedRandomSampler(train_iter.weights, len(train_iter.weights))
     # train_dataloader = DataLoader(train_iter, batch_size=BATCH_SIZE, sampler=train_sampler)
