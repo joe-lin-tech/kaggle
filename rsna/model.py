308a288e34c07dd1615e6459c9b3fb98b9f6a3f3
joe-lin-tech <joe.tech.lin@gmail.com>
2023-09-08 01:12:52 -0700

    feat: add mask feature extraction and integration with main pipeline

--- rsna/model.py
+++ rsna/model.py
@@ -20,21 +20,50 @@ class MaskPredictor(nn.Module):
         args.sam_checkpoint = MASK_MODEL
         model = sam_model_registry["vit_b"](args).to(DEVICE)
         self.mask_generator = SamAutomaticMaskGenerator(model, pred_iou_thresh=0.4, stability_score_thresh=0.8)
+        
+        backbone = resnet18(ResNet18_Weights.DEFAULT)
+        self.backbone = nn.Sequential(*(list(backbone.children())[:-2]))
+        for param in self.backbone.parameters():
+            param.requires_grad = False
+        for param in self.backbone[-1].parameters():
+            param.requires_grad = True
+
+        self.fcn = nn.Sequential(
+            nn.Linear(512, 256),
+            nn.BatchNorm1d(256),
+            nn.GELU(),
+            nn.Linear(256, 128),
+            nn.BatchNorm1d(128),
+            nn.GELU(),
+            nn.Linear(128, 64),
+            nn.BatchNorm1d(64),
+            nn.GELU()
+        )
 
     def forward(self, x):
         for b in range(x.shape[0]):
-            size = 36 # 12
+            size = 24 # 12
             for i in range(size // 2, N_CHANNELS - (size // 2), size):
                 image = x[b, i - 1:i + 2, :, :].transpose(0, 1).transpose(1, 2)
                 # plt.imshow(image[:, :, 1], cmap='bone')
                 masks = self.mask_generator.generate(image)
-                mask = torch.tensor(np.where(np.logical_or.reduce([mask['segmentation'] for mask in masks]), 1, 0.05)).to(x.device)
+                mask = torch.tensor(np.where(np.logical_or.reduce([mask['segmentation'] for mask in masks]), 1, 0)).to(x.device)
                 x[b, i - (size // 2):i + (size // 2), :, :] *= mask
                 # self.show_mask(mask, plt.gca())
                 # for mask in masks:
                 #     self.show_mask(mask['segmentation'], plt.gca())
                 # plt.axis('off')
                 # plt.show()
+        b, c, h, w = x.shape
+        x = torch.reshape(x, (b * (c // 3), 3, h, w))
+        x = self.backbone(x)
+        x = F.adaptive_avg_pool2d(x, 1)
+        x = torch.flatten(x, 1)
+        x = self.fcn(x)
+        x = torch.reshape(x, (b, c // 3, -1))
+        x = torch.transpose(x, 1, 2)
+        x = F.adaptive_avg_pool1d(x, 1)
+        x = torch.flatten(x, 1)
         return x
 
     def show_mask(self, mask, ax):
@@ -42,39 +71,6 @@ class MaskPredictor(nn.Module):
         h, w = mask.shape[-2:]
         mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
         ax.imshow(mask_image)
-
-
-class SlicePredictor(nn.Module):
-    def __init__(self):
-        super(SlicePredictor, self).__init__()
-        backbone = resnet18(weights=ResNet18_Weights.DEFAULT)
-        self.backbone = nn.Sequential(*(list(backbone.children())[:-2]))
-        for param in self.backbone.parameters():
-            param.requires_grad = False
-        for param in self.backbone[-1].parameters():
-            param.requires_grad = True
-
-        self.layer_norm = nn.LayerNorm(512)
-        self.pos_embedding = nn.Parameter(torch.randn(N_CHANNELS // 3, 512))
-
-        encoder_layer = nn.TransformerEncoderLayer(d_model=512, nhead=8)
-        self.encoder = nn.TransformerEncoder(encoder_layer, num_layers=3)
-
-        self.linear = nn.Linear(512, 1)
-
-    def forward(self, x):
-        b, c, h, w = x.shape
-        x = x.view(b * (c // 3), 3, h, w)
-        x = self.backbone(x)
-        x = F.adaptive_avg_pool2d(x, 1)
-        x = torch.flatten(x, 1)
-        x = torch.reshape(x, (b, (c // 3), 512))
-        x = self.layer_norm(x) + self.pos_embedding
-        x = self.encoder(x)
-        x = torch.squeeze(self.linear(x), dim=-1)
-        x = torch.sigmoid(x)
-        x = torch.squeeze(F.interpolate(x.unsqueeze(1), size=c, mode='linear'), dim=1)
-        return x
 
 
 class CombinedLoss(nn.Module):
@@ -106,8 +102,8 @@ class TraumaDetector(nn.Module):
     def __init__(self):
         super(TraumaDetector, self).__init__()
 
-        # self.mask_predictor = MaskPredictor()
-        self.slice_predictor = SlicePredictor()
+        self.mask_predictor = MaskPredictor()
+        # self.slice_predictor = SlicePredictor()
 
         backbone = resnet50(weights=ResNet50_Weights.IMAGENET1K_V2)
         self.backbone = nn.Sequential(*(list(backbone.children())[:-2]))
@@ -132,27 +128,36 @@ class TraumaDetector(nn.Module):
             # nn.Dropout(0.4)
         )
 
-        # self.out_bowel = nn.Linear(64, 1)
-        # self.out_extravasation = nn.Linear(64, 1)
-        self.out_kidney = nn.Linear(64, 3)
-        self.out_liver = nn.Linear(64, 3)
-        self.out_spleen = nn.Linear(64, 3)
-        # self.out_any = nn.Linear(64, 1)
+        self.out = nn.Sequential(
+            nn.Linear(128, 64),
+            nn.BatchNorm1d(64),
+            nn.GELU(),
+            nn.Linear(64, 32),
+            nn.BatchNorm1d(32),
+            nn.GELU()
+        )
+
+        # self.out_bowel = nn.Linear(32, 1)
+        # self.out_extravasation = nn.Linear(32, 1)
+        self.out_kidney = nn.Linear(32, 3)
+        self.out_liver = nn.Linear(32, 3)
+        self.out_spleen = nn.Linear(32, 3)
+        # self.out_any = nn.Linear(32, 1)
     
     def forward(self, x):
-        # x = self.mask_predictor(x)
         b, c, h, w = x.shape
-        prob = self.slice_predictor(x)
-        for _ in range(b):
-            indices = torch.where(prob[_] > 0.5)[0]
-            # start, end = 0, c
-            if indices.shape[0] > 0:
-                # start, end = indices.min().item(), indices.max().item()
-                x[_] = torch.squeeze(F.interpolate(
-                    # torch.unsqueeze(torch.unsqueeze(x[_, start:end + 1], dim=0), dim=0),
-                    torch.unsqueeze(torch.unsqueeze(x[_, indices], dim=0), dim=0),
-                    size=(c, h, w), mode='trilinear'
-                ), dim=(0, 1))
+        mask_features = self.mask_predictor(x.clone())
+        # prob = self.slice_predictor(x)
+        # for _ in range(b):
+        #     indices = torch.where(prob[_] > 0.5)[0]
+        #     # start, end = 0, c
+        #     if indices.shape[0] > 0:
+        #         # start, end = indices.min().item(), indices.max().item()
+        #         x[_] = torch.squeeze(F.interpolate(
+        #             # torch.unsqueeze(torch.unsqueeze(x[_, start:end + 1], dim=0), dim=0),
+        #             torch.unsqueeze(torch.unsqueeze(x[_, indices], dim=0), dim=0),
+        #             size=(c, h, w), mode='trilinear'
+        #         ), dim=(0, 1))
 
         x = torch.reshape(x, (b * (c // 3), 3, h, w))
         x = self.backbone(x)
@@ -160,6 +165,8 @@ class TraumaDetector(nn.Module):
         x = self.head(x)
         x = F.adaptive_avg_pool3d(x, 1)
         x = torch.flatten(x, 1)
+        x = torch.cat([x, mask_features], dim=1)
+        x = self.out(x)
         out = {
             # 'bowel': self.out_bowel(x),
             # 'extravasation': self.out_extravasation(x),
