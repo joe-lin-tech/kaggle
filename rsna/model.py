a8931ac567e5eecf1e033597b4a74013f30b5583
joe-lin-tech <joe.tech.lin@gmail.com>
2023-09-09 16:44:49 -0700

    update: temporarily remove bowel and extravasation head

--- rsna/model.py
+++ rsna/model.py
@@ -57,18 +57,18 @@ class CombinedLoss(nn.Module):
         self.any = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([6]).to(DEVICE))
     
     def forward(self, out, labels):
-        bce_loss = self.bowel(out['bowel'], labels[:, 0:1].float()) + self.extravasation(out['extravasation'], labels[:, 1:2].float())
+        # bce_loss = self.bowel(out['bowel'], labels[:, 0:1].float()) + self.extravasation(out['extravasation'], labels[:, 1:2].float())
         ce_loss = self.kidney(out['kidney'], labels[:, 2]) + self.liver(out['liver'], labels[:, 3]) + self.spleen(out['spleen'], labels[:, 4])
         
-        bowel, extravasation = torch.sigmoid(out['bowel']), torch.sigmoid(out['extravasation'])
-        kidney, liver, spleen = F.softmax(out['kidney'], dim=-1), F.softmax(out['liver'], dim=-1), F.softmax(out['spleen'], dim=-1)
-        any_injury = torch.hstack([bowel, extravasation, kidney[:, 0:1], liver[:, 0:1], spleen[:, 0:1]])
-        any_injury, _ = torch.max(1 - any_injury, keepdim=True, dim=-1)
-        any_loss = self.any(any_injury, labels[:, 5:6].float())
+        # bowel, extravasation = torch.sigmoid(out['bowel']), torch.sigmoid(out['extravasation'])
+        # kidney, liver, spleen = F.softmax(out['kidney'], dim=-1), F.softmax(out['liver'], dim=-1), F.softmax(out['spleen'], dim=-1)
+        # any_injury = torch.hstack([bowel, extravasation, kidney[:, 0:1], liver[:, 0:1], spleen[:, 0:1]])
+        # any_injury, _ = torch.max(1 - any_injury, keepdim=True, dim=-1)
+        # any_loss = self.any(any_injury, labels[:, 5:6].float())
         # any_loss = self.any(out['any'], labels[:, 5:6].float())
 
-        return bce_loss + ce_loss + any_loss
-        # return ce_loss
+        # return bce_loss + ce_loss + any_loss
+        return ce_loss
     
 
 class TraumaDetector(nn.Module):
@@ -111,8 +111,8 @@ class TraumaDetector(nn.Module):
             nn.GELU()
         )
 
-        self.out_bowel = nn.Linear(32, 1)
-        self.out_extravasation = nn.Linear(32, 1)
+        # self.out_bowel = nn.Linear(32, 1)
+        # self.out_extravasation = nn.Linear(32, 1)
         self.out_kidney = nn.Linear(32, 3)
         self.out_liver = nn.Linear(32, 3)
         self.out_spleen = nn.Linear(32, 3)
@@ -131,8 +131,8 @@ class TraumaDetector(nn.Module):
         x = torch.cat([x, mask_features], dim=1)
         x = self.out(x)
         out = {
-            'bowel': self.out_bowel(x),
-            'extravasation': self.out_extravasation(x),
+            # 'bowel': self.out_bowel(x),
+            # 'extravasation': self.out_extravasation(x),
             'kidney': self.out_kidney(x),
             'liver': self.out_liver(x),
             'spleen': self.out_spleen(x),
