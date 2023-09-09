4f8994d928b287e7c850846ec91e54a33d5c261d
joe-lin-tech <joe.tech.lin@gmail.com>
2023-09-08 23:06:07 -0700

    fix: correct dict keys for batched input

--- rsna/dataset.py
+++ rsna/dataset.py
@@ -45,7 +45,7 @@ class RSNADataset(Dataset):
         cols = self.patient_df.iloc[idx].to_numpy()[1:]
         label = np.hstack([np.argmax(cols[0:2], keepdims=True), np.argmax(cols[2:4], keepdims=True), np.argmax(cols[4:7]), np.argmax(cols[7:10]), np.argmax(cols[10:]),
                            0 if cols[0] == 1 and cols[2] == 1 and cols[4] == 1 and cols[7] == 1 and cols[10] == 1 else 1])
-        return { 'scans': input, 'masked_scans': masked_input, 'label': label }
+        return { 'scans': input, 'masked_scans': masked_input, 'labels': label }
     
     def apply_masks(self, idx, input):
         size = 6 # 12
