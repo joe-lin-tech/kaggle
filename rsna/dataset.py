49ee38d45cc6986dd099c202b600c9035fce0720
joe-lin-tech <joe.tech.lin@gmail.com>
2023-09-09 11:47:11 -0700

    update: increase dropout regularization and fix mask file names

--- rsna/dataset.py
+++ rsna/dataset.py
@@ -39,7 +39,7 @@ class RSNADataset(Dataset):
         input = images[0] # fix sample selection
 
         input = self.transform['preprocess'](torch.tensor(input).float())
-        masked_input = self.apply_masks(str(idx), input.clone())
+        masked_input = self.apply_masks(str(self.patient_df.iloc[idx].patient_id), input.clone())
         
         if self.mode == 'train':
             transformed_input = self.transform['random'](torch.concat([input, masked_input], dim=0))
@@ -51,10 +51,10 @@ class RSNADataset(Dataset):
                            0 if cols[0] == 1 and cols[2] == 1 and cols[4] == 1 and cols[7] == 1 and cols[10] == 1 else 1])
         return { 'scans': input, 'masked_scans': masked_input, 'labels': label }
     
-    def apply_masks(self, idx, input):
+    def apply_masks(self, id, input):
         size = 6 # 12
-        if idx + '.npz' in os.listdir(os.path.join(MASK_FOLDER, self.mode)):
-            masks = np.load(os.path.join(MASK_FOLDER, self.mode, idx + '.npz'))
+        if id + '.npz' in os.listdir(os.path.join(MASK_FOLDER, self.mode)):
+            masks = np.load(os.path.join(MASK_FOLDER, self.mode, id + '.npz'))
             for i in range(size // 2, N_CHANNELS, size):
                 input[i - (size // 2):i + (size // 2), :, :] *= masks[str(i)]
         else:
@@ -65,7 +65,7 @@ class RSNADataset(Dataset):
                 mask = np.where(np.logical_or.reduce([mask['segmentation'] for mask in masks]), 1, 0)
                 input[i - (size // 2):i + (size // 2), :, :] *= mask
                 save_masks[str(i)] = mask
-            np.savez(os.path.join(MASK_FOLDER, self.mode, idx + '.npz'), **save_masks)
+            np.savez(os.path.join(MASK_FOLDER, self.mode, id + '.npz'), **save_masks)
         return input
     
     def show_mask(self, mask, ax):
