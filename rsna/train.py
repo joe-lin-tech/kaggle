85d4a966d89374f64f9a3d2b8201bcc1851c75d6
joe-lin-tech <joe.tech.lin@gmail.com>
2023-09-11 07:25:16 -0700

    update: change training hyperparameters

--- rsna/train.py
+++ rsna/train.py
@@ -137,13 +137,13 @@ for i, (train_idx, val_idx) in enumerate(splits):
         ]), 'lr': OUT_LR }
     ]
     # optimizer = torch.optim.SGD(model.parameters(), lr=LEARNING_RATE, momentum=0.9, weight_decay=1e-2)
-    optimizer = torch.optim.SGD(model_lr, momentum=0.9, weight_decay=1e-2)
+    optimizer = torch.optim.SGD(model_lr, momentum=0.9, weight_decay=1e-3)
     # optimizer = torch.optim.AdamW(model.parameters(), lr=LEARNING_RATE)
     if args.checkpoint:
         optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
     # scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, 10, eta_min=1e-3)
     # scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=3)
-    scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
+    scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.99)
     # scheduler = None
 
     loss_fn = CombinedLoss()
