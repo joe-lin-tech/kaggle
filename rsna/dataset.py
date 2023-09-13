8473fc9ac1394381638a286dae6f02fa9c4d11a0
joe-lin-tech <joe.tech.lin@gmail.com>
2023-09-13 15:45:31 -0700

    fix: correct loss function implementation

--- rsna/dataset.py
+++ rsna/dataset.py
@@ -75,7 +75,7 @@ class RSNADataset(Dataset):
         #     masked_input = transformed_input[N_CHANNELS:]
 
         cols = self.patient_df.iloc[idx].to_numpy()[1:]
-        label = np.hstack([np.argmax(cols[0:2], keepdims=True), np.argmax(cols[2:4], keepdims=True), np.argmax(cols[4:7]), np.argmax(cols[7:10]), np.argmax(cols[10:]),
+        label = np.hstack([np.argmax(cols[0:2], keepdims=True), np.argmax(cols[2:4], keepdims=True), cols[4:7], cols[7:10], cols[10:],
                            0 if cols[0] == 1 and cols[2] == 1 and cols[4] == 1 and cols[7] == 1 and cols[10] == 1 else 1])
         return { 'scans': input, 'masked_scans': masked_input, 'labels': label }
     
--- rsna/model.py
+++ rsna/model.py
@@ -57,26 +57,26 @@ class MaskEncoder(nn.Module):
 class CombinedLoss(nn.Module):
     def __init__(self):
         super(CombinedLoss, self).__init__()
-        self.bowel = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([2]).to(DEVICE))
-        self.extravasation = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([6]).to(DEVICE))
+        # self.bowel = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([2]).to(DEVICE))
+        # self.extravasation = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([6]).to(DEVICE))
         self.kidney = nn.CrossEntropyLoss(weight=torch.tensor([1.0, 2.0, 4.0]).to(DEVICE))
         self.liver = nn.CrossEntropyLoss(weight=torch.tensor([1.0, 2.0, 4.0]).to(DEVICE))
         self.spleen = nn.CrossEntropyLoss(weight=torch.tensor([1.0, 2.0, 4.0]).to(DEVICE))
-        self.any = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([6]).to(DEVICE))
+        # self.any = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([6]).to(DEVICE))
     
     def forward(self, out, labels):
-        bce_loss = self.bowel(out['bowel'], labels[:, 0:1].float()) + self.extravasation(out['extravasation'], labels[:, 1:2].float())
-        ce_loss = self.kidney(out['kidney'], labels[:, 2]) + self.liver(out['liver'], labels[:, 3]) + self.spleen(out['spleen'], labels[:, 4])
+        # bce_loss = self.bowel(out['bowel'], labels[:, 0:1].float()) + self.extravasation(out['extravasation'], labels[:, 1:2].float())
+        ce_loss = self.kidney(out['kidney'], labels[:, 2:5].float()) + self.liver(out['liver'], labels[:, 5:8].float()) + self.spleen(out['spleen'], labels[:, 8:11].float())
         
-        bowel, extravasation = torch.sigmoid(out['bowel']), torch.sigmoid(out['extravasation'])
-        kidney, liver, spleen = F.softmax(out['kidney'], dim=-1), F.softmax(out['liver'], dim=-1), F.softmax(out['spleen'], dim=-1)
-        healthy = torch.hstack([1 - bowel, 1 - extravasation, kidney[:, 0:1], liver[:, 0:1], spleen[:, 0:1]])
-        any_injury, _ = torch.max(1 - healthy, keepdim=True, dim=-1)
-        any_loss = self.any(any_injury, labels[:, 5:6].float())
+        # bowel, extravasation = torch.sigmoid(out['bowel']), torch.sigmoid(out['extravasation'])
+        # kidney, liver, spleen = F.softmax(out['kidney'], dim=-1), F.softmax(out['liver'], dim=-1), F.softmax(out['spleen'], dim=-1)
+        # healthy = torch.hstack([1 - bowel, 1 - extravasation, kidney[:, 0:1], liver[:, 0:1], spleen[:, 0:1]])
+        # any_injury, _ = torch.max(1 - healthy, keepdim=True, dim=-1)
+        # any_loss = self.any(any_injury, labels[:, 5:6].float())
         # any_loss = self.any(out['any'], labels[:, 5:6].float())
 
-        return bce_loss + ce_loss + any_loss
-        # return ce_loss
+        # return bce_loss + ce_loss + any_loss
+        return ce_loss
     
 
 class TraumaDetector(nn.Module):
