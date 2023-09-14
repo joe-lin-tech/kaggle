fix: remove gradient clipping

rsna/train.py
@@ -71,8 +71,8 @@ def train_epoch(train_dataloader, model, optimizer, scheduler):
         scaler.scale(loss).backward()
 
         if ((i + 1) % ACCUM_ITER == 0) or (i + 1 == len(train_dataloader)):
-            scaler.unscale_(optimizer)
-            torch.nn.utils.clip_grad_norm_(model.parameters(), GRAD_CLIP_NORM)
+            # scaler.unscale_(optimizer)
+            # torch.nn.utils.clip_grad_norm_(model.parameters(), GRAD_CLIP_NORM)
 
             scaler.step(optimizer)
             scaler.update()