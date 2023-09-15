fix: correct batch normalization dimensions

rsna/model.py
@@ -33,15 +33,15 @@ class MaskEncoder(nn.Module):
 
         self.fcn = nn.Sequential(
             nn.Linear(768, 256),
-            nn.BatchNorm1d(256),
+            nn.BatchNorm1d(32),
             nn.ReLU(),
             nn.Dropout1d(),
             nn.Linear(256, 128),
-            nn.BatchNorm1d(128),
+            nn.BatchNorm1d(32),
             nn.ReLU(),
             nn.Dropout1d(),
             nn.Linear(128, 64),
-            nn.BatchNorm1d(64),
+            nn.BatchNorm1d(32),
             nn.ReLU(),
             # nn.Dropout()
         )