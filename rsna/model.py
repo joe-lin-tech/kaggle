241ff835aee39d6f1eae5f5976971fb58a9ae5a1
joe-lin-tech <joe.tech.lin@gmail.com>
2023-09-15 12:25:22 -0700

    update: change model architecture and loss

--- rsna/model.py
+++ rsna/model.py
@@ -97,25 +97,25 @@ class SlicePredictor(nn.Module):
 class CombinedLoss(nn.Module):
     def __init__(self):
         super(CombinedLoss, self).__init__()
-        self.bowel = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([2]).to(DEVICE))
-        self.extravasation = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([6]).to(DEVICE))
+        # self.bowel = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([2]).to(DEVICE))
+        # self.extravasation = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([6]).to(DEVICE))
         self.kidney = nn.CrossEntropyLoss(weight=torch.tensor([1.0, 2.0, 4.0]).to(DEVICE))
         self.liver = nn.CrossEntropyLoss(weight=torch.tensor([1.0, 2.0, 4.0]).to(DEVICE))
         self.spleen = nn.CrossEntropyLoss(weight=torch.tensor([1.0, 2.0, 4.0]).to(DEVICE))
-        self.any = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([6]).to(DEVICE))
+        # self.any = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([6]).to(DEVICE))
     
     def forward(self, out, labels):
-        bce_loss = self.bowel(out['bowel'], labels[:, 0:1].float()) + self.extravasation(out['extravasation'], labels[:, 1:2].float())
+        # bce_loss = self.bowel(out['bowel'], labels[:, 0:1].float()) + self.extravasation(out['extravasation'], labels[:, 1:2].float())
         ce_loss = self.kidney(out['kidney'], labels[:, 2:5].float()) + self.liver(out['liver'], labels[:, 5:8].float()) + self.spleen(out['spleen'], labels[:, 8:11].float())
         
-        bowel, extravasation = torch.sigmoid(out['bowel']), torch.sigmoid(out['extravasation'])
-        kidney, liver, spleen = F.softmax(out['kidney'], dim=-1), F.softmax(out['liver'], dim=-1), F.softmax(out['spleen'], dim=-1)
-        healthy = torch.hstack([1 - bowel, 1 - extravasation, kidney[:, 0:1], liver[:, 0:1], spleen[:, 0:1]])
-        any_injury, _ = torch.max(1 - healthy, keepdim=True, dim=-1)
-        any_loss = self.any(any_injury, labels[:, 5:6].float())
+        # bowel, extravasation = torch.sigmoid(out['bowel']), torch.sigmoid(out['extravasation'])
+        # kidney, liver, spleen = F.softmax(out['kidney'], dim=-1), F.softmax(out['liver'], dim=-1), F.softmax(out['spleen'], dim=-1)
+        # healthy = torch.hstack([1 - bowel, 1 - extravasation, kidney[:, 0:1], liver[:, 0:1], spleen[:, 0:1]])
+        # any_injury, _ = torch.max(1 - healthy, keepdim=True, dim=-1)
+        # any_loss = self.any(any_injury, labels[:, 5:6].float())
 
-        return bce_loss + ce_loss + any_loss
-        # return ce_loss
+        # return bce_loss + ce_loss + any_loss
+        return ce_loss
     
 
 class TraumaDetector(nn.Module):
@@ -150,22 +150,22 @@ class TraumaDetector(nn.Module):
             # nn.Dropout(0.4)
         )
 
-        self.out = nn.Sequential(
-            # nn.Linear(128, 64),
-            # nn.BatchNorm1d(64),
-            # nn.ReLU(),
-            # nn.Dropout(),
-            nn.Linear(64, 32),
-            nn.BatchNorm1d(32),
-            nn.ReLU(),
-            # nn.Dropout()
-        )
+        # self.out = nn.Sequential(
+        #     # nn.Linear(128, 64),
+        #     # nn.BatchNorm1d(64),
+        #     # nn.ReLU(),
+        #     # nn.Dropout(),
+        #     nn.Linear(64, 32),
+        #     nn.BatchNorm1d(32),
+        #     nn.ReLU(),
+        #     # nn.Dropout()
+        # )
 
-        self.out_bowel = nn.Linear(32, 1)
-        self.out_extravasation = nn.Linear(32, 1)
-        self.out_kidney = nn.Linear(32, 3)
-        self.out_liver = nn.Linear(32, 3)
-        self.out_spleen = nn.Linear(32, 3)
+        # self.out_bowel = nn.Linear(32, 1)
+        # self.out_extravasation = nn.Linear(32, 1)
+        self.out_kidney = nn.Linear(64, 3)
+        self.out_liver = nn.Linear(64, 3)
+        self.out_spleen = nn.Linear(64, 3)
     
     def forward(self, scans, masked_scans):
         b, c, h, w = scans.shape
@@ -186,11 +186,11 @@ class TraumaDetector(nn.Module):
         x = F.adaptive_avg_pool3d(x, 1)
         x = torch.flatten(x, 1)
         # x = torch.cat([x, mask_features], dim=1)
-        x = self.out(x)
+        # x = self.out(x)
         # x = self.out(mask_features)
         out = {
-            'bowel': self.out_bowel(x),
-            'extravasation': self.out_extravasation(x),
+            # 'bowel': self.out_bowel(x),
+            # 'extravasation': self.out_extravasation(x),
             'kidney': self.out_kidney(x),
             'liver': self.out_liver(x),
             'spleen': self.out_spleen(x)
