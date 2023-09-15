33f8ef041474677e6dbd900008b32f0ccc648fac
joe-lin-tech <joe.tech.lin@gmail.com>
2023-09-15 03:18:21 -0700

    update: reduce model architecture

--- rsna/model.py
+++ rsna/model.py
@@ -36,13 +36,10 @@ class MaskEncoder(nn.Module):
             nn.BatchNorm1d(256),
             nn.ReLU(),
             nn.Dropout1d(),
-            nn.Linear(256, 128),
-            nn.BatchNorm1d(128),
-            nn.ReLU(),
-            nn.Dropout1d(),
-            nn.Linear(128, 64),
+            nn.Linear(256, 64),
             nn.BatchNorm1d(64),
             nn.ReLU(),
+            nn.Dropout1d(),
             # nn.Dropout()
         )
 
