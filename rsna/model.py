fix: change adaptive average pooling to 1d

rsna/model.py
@@ -50,7 +50,7 @@ class MaskEncoder(nn.Module):
         # x = torch.flatten(x, 1)
         x = torch.reshape(x, (b, c // 3, -1))
         x = torch.transpose(x, 1, 2)
-        x = F.adaptive_avg_pool2d(x, 1)
+        x = F.adaptive_avg_pool1d(x, 1)
         x = self.fcn(x)
         return x
 