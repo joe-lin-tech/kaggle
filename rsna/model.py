b93fc99cd3170914a5ed0153bdac2c685e8894ef
joe-lin-tech <joe.tech.lin@gmail.com>
2023-09-15 21:24:09 -0700

    fix: flatten tensor before fully connected network

--- rsna/model.py
+++ rsna/model.py
@@ -51,6 +51,7 @@ class MaskEncoder(nn.Module):
         x = torch.reshape(x, (b, c // 3, -1))
         x = torch.transpose(x, 1, 2)
         x = F.adaptive_avg_pool1d(x, 1)
+        x = torch.flatten(x, 1)
         x = self.fcn(x)
         return x
 
