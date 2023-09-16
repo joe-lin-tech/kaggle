fix: test torch cat to correct gradient flow

rsna/model.py
@@ -180,8 +180,10 @@ class TraumaDetector(nn.Module):
                     size=(c, h, w), mode='trilinear'
                 ), dim=(0, 1))
                 x.append(interpolated_scans)
+            else:
+                x.append(scans[_])
 
-        x = torch.reshape(torch.stack(x, dim=0), (b * (c // 3), 3, h, w))
+        x = torch.reshape(torch.cat(torch.unsqueeze(x, dim=0), dim=0), (b * (c // 3), 3, h, w))
         x = self.backbone(x)
         x = torch.reshape(x, (b, c // 3, x.shape[-3], x.shape[-2], x.shape[-1])).transpose(1, 2)
         x = self.head(x)