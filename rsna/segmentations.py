4be5fe4317a091adf93500cfaefd9aa7e1a58062
joe-lin-tech <joe.tech.lin@gmail.com>
2023-09-17 12:20:52 -0700

    update: add progress for segmentations

--- rsna/segmentations.py
+++ rsna/segmentations.py
@@ -1,10 +1,11 @@
 from totalsegmentator.python_api import totalsegmentator
 import pandas as pd
 import os
+from tqdm import tqdm
 from params import *
 
 data = pd.read_csv(CSV_FILE)
-for i in range(len(data)):
+for i in tqdm(range(len(data))):
     path = os.path.join(ROOT_DIR, str(data.iloc[i].patient_id))
     images = []
     for root, dirs, _ in os.walk(path):
