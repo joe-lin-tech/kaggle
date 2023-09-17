fix: average loss values

rsna/model.py
@@ -98,7 +98,7 @@ class CombinedLoss(nn.Module):
         healthy = torch.cat([kidney[:, 0:1], liver[:, 0:1], spleen[:, 0:1]], dim=-1)
         any_injury, _ = torch.max(1 - healthy, keepdim=True, dim=-1)
         any_injury = torch.clamp(any_injury, 1e-7, 1 - 1e-7)
-        any_loss = torch.neg(6 * labels[:, 11:12] * torch.log(any_injury) + (1 - labels[:, 11:12]) * torch.log(1 - any_injury))
+        any_loss = torch.mean(torch.neg(6 * labels[:, 11:12] * torch.log(any_injury) + (1 - labels[:, 11:12]) * torch.log(1 - any_injury)))
         return ce_loss + any_loss
     
 