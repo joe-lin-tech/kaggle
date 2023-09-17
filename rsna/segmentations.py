e58d2fb171c0806ec84b1d16df4a6182c91b96ce
joe-lin-tech <joe.tech.lin@gmail.com>
2023-09-17 13:13:23 -0700

    update: change segmentation script

--- rsna/segmentations.py
+++ rsna/segmentations.py
@@ -11,11 +11,11 @@ for i in tqdm(range(len(data))):
     images = []
     for root, dirs, _ in os.walk(path):
         for dirname in dirs:
-            os.makedirs(f"masks/total_seg/{str(data.iloc[i].patient_id)}")
-            subprocess.call(f"""TotalSegmentator -i {os.path.join(root, dirname)}
-                            -o masks/total_seg/{str(data.iloc[i].patient_id)}/{dirname}.nii.gz
-                            -ot 'nifti' -rs spleen kidney_left kidney_right liver esophagus colon duodenum small_bowel stomach
-                            -ml --fast""", shell=True)
+            # os.makedirs(f"masks/total_seg/{str(data.iloc[i].patient_id)}")
+            input = os.path.join(root, dirname)
+            output = f"masks/total_seg/{str(data.iloc[i].patient_id)}/{dirname}.nii.gz"
+            subset = "spleen kidney_left kidney_right liver esophagus colon duodenum small_bowel stomach"
+            subprocess.call(f"TotalSegmentator -i {input} -o {output} -ot 'nifti' -rs {subset} -ml --fast", shell=True)
             # totalsegmentator(os.path.join(root, dirname), 'masks/total_seg',
             #                  output_type='nifti', roi_subset='spleen kidney_left kidney_right liver esophagus colon duodenum small_bowel stomach',
             #                  fast=True, ml=True, preview=True)
