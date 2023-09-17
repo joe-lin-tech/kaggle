32b38b5aa4758f93dd763b534955233fdd218be0
joe-lin-tech <joe.tech.lin@gmail.com>
2023-09-17 12:19:32 -0700

    feat: add total segmentator masks

---
+++ rsna/segmentations.py
@@ -0,0 +1,12 @@
+from totalsegmentator.python_api import totalsegmentator
+import pandas as pd
+import os
+from params import *
+
+data = pd.read_csv(CSV_FILE)
+for i in range(len(data)):
+    path = os.path.join(ROOT_DIR, str(data.iloc[i].patient_id))
+    images = []
+    for root, dirs, _ in os.walk(path):
+        for dirname in dirs:
+            totalsegmentator(os.path.join(root, dirname), 'masks/total_seg', output_type='nifti', roi_subset='spleen kidney_left kidney_right liver esophagus colon duodenum small_bowel stomach')
