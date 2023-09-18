fix: correct model loss weighting

rsna/model.py
@@ -132,20 +132,24 @@ class SegmentationNet(nn.Module):
 class CombinedLoss(nn.Module):
     def __init__(self):
         super(CombinedLoss, self).__init__()
-        self.kidney = nn.CrossEntropyLoss(weight=torch.tensor([1.0, 2.0, 4.0]).to(DEVICE))
-        self.liver = nn.CrossEntropyLoss(weight=torch.tensor([1.0, 2.0, 4.0]).to(DEVICE))
-        self.spleen = nn.CrossEntropyLoss(weight=torch.tensor([1.0, 2.0, 4.0]).to(DEVICE))
+        self.kidney = nn.CrossEntropyLoss(reduction='none')
+        self.liver = nn.CrossEntropyLoss(reduction='none')
+        self.spleen = nn.CrossEntropyLoss(reduction='none')
+        self.organ_weights = torch.tensor([[1, 2, 4]]).T
+        self.any_weights = torch.tensor([[6]])
     
     def forward(self, out, labels):
         kidney, liver, spleen = out
-        ce_loss = self.kidney(kidney, labels[:, 2:5].float()) + self.liver(liver, labels[:, 5:8].float()) + self.spleen(spleen, labels[:, 8:11].float())
+        ce_loss = self.kidney(kidney, labels[:, 2:5].float()) * (labels[:, 2:5] @ self.organ_weights) \
+            + self.liver(liver, labels[:, 5:8].float()) * (labels[:, 5:8] @ self.organ_weights) \
+                + self.spleen(spleen, labels[:, 8:11].float()) * (labels[:, 8:11] @ self.organ_weights)
 
         kidney, liver, spleen = F.softmax(kidney, dim=-1), F.softmax(liver, dim=-1), F.softmax(spleen, dim=-1)
         healthy = torch.cat([kidney[:, 0:1], liver[:, 0:1], spleen[:, 0:1]], dim=-1)
         any_injury, _ = torch.max(1 - healthy, keepdim=True, dim=-1)
         any_injury = torch.clamp(any_injury, 1e-7, 1 - 1e-7)
-        any_loss = torch.mean(torch.neg(6 * labels[:, 11:12] * torch.log(any_injury) + (1 - labels[:, 11:12]) * torch.log(1 - any_injury)))
-        return ce_loss + any_loss
+        any_loss = torch.neg(labels[:, 11:12] * torch.log(any_injury) + (1 - labels[:, 11:12]) * torch.log(1 - any_injury)) * (labels[:, 11:12] @ self.any_weights + 1)
+        return torch.mean(ce_loss + any_loss)
     
 
 class TraumaDetector(nn.Module):
@@ -195,5 +199,5 @@ class TraumaDetector(nn.Module):
         kidney = self.out_kidney(x)
         liver = self.out_liver(x)
         spleen = self.out_spleen(x)
-        
+
         return kidney, liver, spleen