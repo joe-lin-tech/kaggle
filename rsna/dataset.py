fix: correct mean and std computation

rsna/dataset.py
@@ -138,13 +138,6 @@ class RSNADataset(Dataset):
         self.mode = mode
         self.input_type = input_type
 
-        # TODO: lines to temporarily compute dataset mean and variance
-        self.slice_mean = 0
-        self.slice_var = 0
-        self.mask_mean = 0
-        self.mask_var = 0
-        self.compute_count = 0
-
     
     def __len__(self):
         return len(self.patient_df)
@@ -155,16 +148,6 @@ class RSNADataset(Dataset):
         save_file = str(self.patient_df.iloc[idx].patient_id) + '.npy'
         if save_file in os.listdir(TEMP_DIR):
             input = torch.tensor(np.load(os.path.join(TEMP_DIR, save_file)))
-
-            # TODO: lines to temporarily compute dataset mean and variance
-            slices = input[torch.arange(input.shape[0]) % SLICE_CHANNELS != 0]
-            self.slice_mean += torch.mean(slices)
-            self.slice_var += torch.var(slices)
-            masks = input[SLICE_CHANNELS - 1::SLICE_CHANNELS]
-            self.mask_mean += torch.mean(masks)
-            self.mask_var += torch.var(masks)
-            self.compute_count += 1
-
         else:
             images = []
             for root, dirs, _ in os.walk(path):
@@ -253,9 +236,27 @@ class RSNADataset(Dataset):
     
     # TODO: method to temporarily compute dataset mean and variance
     def compute_mean_std(self):
-        slice_mean = self.slice_mean / self.compute_count
-        slice_std = torch.sqrt(self.slice_var / self.compute_count)
-        mask_mean = self.mask_mean / self.compute_count
-        mask_std = torch.sqrt(self.mask_var / self.compute_count)
-        print(self.slice_mean, self.slice_var, self.mask_mean, self.mask_var, self.compute_count)
+        slice_mean, slice_var = 0, 0
+        mask_mean, mask_var = 0, 0
+        compute_count = 0
+
+        for i in range(len(self)):
+            save_file = str(self.patient_df.iloc[i].patient_id) + '.npy'
+            if save_file in os.listdir(TEMP_DIR):
+                input = torch.tensor(np.load(os.path.join(TEMP_DIR, save_file)))
+
+                # TODO: lines to temporarily compute dataset mean and variance
+                slices = input[torch.arange(input.shape[0]) % SLICE_CHANNELS != 0]
+                slice_mean += torch.mean(slices)
+                slice_var += torch.var(slices)
+                masks = input[SLICE_CHANNELS - 1::SLICE_CHANNELS]
+                mask_mean += torch.mean(masks)
+                mask_var += torch.var(masks)
+                compute_count += 1
+
+        print(slice_mean, slice_var, mask_mean, mask_var, compute_count)
+        slice_mean /= compute_count
+        slice_std = torch.sqrt(slice_var / compute_count)
+        mask_mean /= compute_count
+        mask_std = torch.sqrt(mask_var / compute_count)
         print(slice_mean, slice_std, mask_mean, mask_std)